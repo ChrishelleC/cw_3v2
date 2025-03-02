28ec019b13a55fcb2a3a90782fc71e6fcc91ecec
ChrishelleC <culmerchrishelle12@gmail.com>
2025-03-01 23:08:18 -0500

    ability to add task, set complete and delete tasks

--- lib/main.dart
+++ lib/main.dart
@@ -30,4 +30,25 @@ class _TaskListState extends State<TaskList> {
   final TextEditingController _taskController = TextEditingController();
   String _selectedPriority = 'Medium';
   List<Task> _tasks = [];
+
+  void _addTask() {
+    if (_taskController.text.isNotEmpty) {
+      setState(() {
+        _task.add(Task(name: _taskController.text, priority: _selectedPriority));
+        _taskController.clear();
+      });
+    }
+  }
+
+  void _toggleTaskCompletion(int index) {
+    setState((){
+      _tasks[index].isCompleted = !_tasks[index].isCompleted;
+    });
+  }
+  
+  void _deleteTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+    });
+  }
 }
